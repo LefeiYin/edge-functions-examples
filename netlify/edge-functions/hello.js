1237fa31c503ac71c8fd779d9eef36bf507a68c8
LefeiYin <127030449+LefeiYin@users.noreply.github.com>
2023-07-07 19:20:45 +0800

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,5 +1,23 @@
-export default async (request) => {
-  return new Response("Hello, World!", {
-    headers: { "content-type": "text/html" },
-  });
+export default {
+  async fetch(request) {
+
+    let pkg = {
+      'apikey':'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',
+      'data': {
+        'model':'gpt-3.5-turbo',
+        'messages':[{'role':'user','content':'你使用的是什么模型？是最新的模型吗'}]
+      }
+    }
+
+    let config = {
+      headers:{
+        'Content-Type':'application/json',
+        'Authorization': 'Bearer '+ pkg.apikey,
+      },
+      method:'POST',
+      body:JSON.stringify(pkg.data)
+    }
+
+    return fetch('https://api.openai.com/v1/chat/completions', config);
+  },
 };
